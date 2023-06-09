d8d1cc10ef597f61bb7da23dab6ffccc2de33b6b
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-06-09 11:52:42 +0300

    setup custom session and registration devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrationS: 'users/registrations'},
+    path: 'auth', 
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
